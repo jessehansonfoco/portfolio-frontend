df6f2d0b758e9ef448bb3d9f3823e5ce0d164b33
Jesse Hanson <jhanson@2ndswing.com>
2025-07-13 13:17:22 -0600

    adjust title

--- src/views/frontend/Index.vue
+++ src/views/frontend/Index.vue
@@ -51,7 +51,7 @@ fetchData()
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto max-w-2xl lg:mx-0">
         <h2 class="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">Jesse Hanson</h2>
-        <p class="mt-2 text-lg/8 text-gray-600">Full Stack Software Developer in Fort Collins, Colorado</p>
+        <p class="mt-2 text-lg/8 text-gray-600">Full Stack Software Developer<br>Fort Collins, Colorado</p>
       </div>
       <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
         <article v-for="post in posts" :key="post.id" class="flex max-w-xl flex-col items-start justify-between">
